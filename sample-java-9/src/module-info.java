5ecfd7a55de4b001a68e0860b65097bee40e8b59
kbhatt23 <kanishklikesprogramming@gmail.com>
2020-10-19 11:22:53 +0530

    Initial java 9 project

---
+++ sample-java-9/src/module-info.java
@@ -0,0 +1,2 @@
+module sampleJavaProjectForLearning {
+}
---
+++ sample-java-9/src/sample/HelloList.java
@@ -0,0 +1,9 @@
+package sample;
+
+import java.util.List;
+
+public class HelloList {
+	public static void main(String[] args) {
+		System.out.println(List.of("sita-ram","radhe-shyam"));
+	}
+}
